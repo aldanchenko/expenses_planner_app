96521557ba62fc27dab369974255af8cd19d77d6
Aleksandr <aldanchenko@gmail.com>
2020-10-08 18:28:37 +0300

    Fix keys issue.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -28,14 +28,13 @@ class TransactionList extends StatelessWidget {
               );
             },
           )
-        : ListView.builder(
-            itemBuilder: (context, index) {
-              return TransactionItem(
-                  transaction: transactions[index],
-                  deleteTransaction: deleteTransaction
-              );
-            },
-            itemCount: transactions.length,
+        : ListView(
+            children: transactions
+                .map((transaction) => TransactionItem(
+                    key: ValueKey(transaction.id),
+                    transaction: transaction,
+                    deleteTransaction: deleteTransaction))
+                .toList(),
           );
   }
 }
